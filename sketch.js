d066a59350ebe6477498822ddd6f12eba1070b8c
Sanvi2306 <72811134+Sanvi2306@users.noreply.github.com>
2020-11-26 18:47:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ground,paper1,dustbin;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+ground=new Ground(400,600,800,30);
+paper1= new Paper(200,550,20);
+dustbin= new Dustbin(600,580);
+
+	
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  
+
+
+ ground.display();
+
+ paper1.display();
+
+ dustbin.display();
+  
+}
+function keyPressed(){
+	if(keyCode=== UP_ARROW){
+	    
+	  Body.applyForce(paper1.body,paper1.body.position,{x:40,y:-85});
+	
+	
+	
+	}
+	}
+
+
